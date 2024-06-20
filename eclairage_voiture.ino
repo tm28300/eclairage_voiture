b8fea76cac1a966f4c2d85bc5e43e68ec50ae14a
Thierry MARTIN <personnel@martin-thierry.nom.fr>
2024-06-20 20:55:30 +0200

    Mise à jour avec la dernière version

--- eclairage_voiture.ino
+++ eclairage_voiture.ino
@@ -4,7 +4,6 @@
     * De clignotants gauche et droite (facultatif - fréquence : 90 par minute). Obligatoires à partir du 1er janvier 1969
     * Les clignotants peuvent être utilisés comme feux de détresse. Obligatoires à partir du 1er octobre 1980.
     * Plusieurs configurations d'éclairages sont possibles. On choisit la configuration d'éclairage utilisé avec une combinaison de connexion, ou non, de plusieurs entrées de l'Arduino.
-    * Un potentiomètre permet de faire varier la durée des différentes étapes de l'enchaînement d'éclairage.
 
   Configurations d'éclairage :
     1/ Lumières de nuit fixes.
@@ -27,42 +26,52 @@
 
   Fils à brancher pour les différentes configurations :
     1/ Aucun.
-    2/ 6.
-    3/ 7.
-    4/ 6 et 7.
-    5/ 8.
-    6/ 6 et 8.
-    7/ 6 et 7.
-    8/ 6, 7 et 8.
+    2/ GND, 6.
+    3/ GND, 7.
+    4/ GND, 6 et 7.
+    5/ GND, 8.
+    6/ GND, 6 et 8.
+    7/ GND, 6 et 7.
+    8/ GND, 6, 7 et 8.
 */
 
 // Connexions
-const uint8_t feu_avant = 3;
-const uint8_t feu_arriere = 5;
-const uint8_t clignotants_gauche = 2;
-const uint8_t clignotants_droite = 4;
-const uint8_t configuration_1 = 6;
-const uint8_t configuration_2 = 7;
-const uint8_t configuration_3 = 8;
-const uint8_t potentiometre_duree = A0;
+#ifdef __AVR_ATtiny85__
+  #define feu_avant 0
+  #define feu_arriere 1
+  #define clignotants_gauche 2
+  #define clignotants_droite 3
+  #define configuration_avec_potentiometre
+  #define configuration_potentiometre_init 4
+  #define configuration_potentiometre_read 2
+#else
+  #define avec_serial
+  #define feu_avant 3
+  #define feu_arriere 5
+  #define clignotants_gauche 2
+  #define clignotants_droite 4
+  #define configuration_1 6
+  #define configuration_2 7
+  #define configuration_3 8
+#endif
 
 // Paramètres des feux
-const uint8_t frequence_clignotant = 90;
-const uint8_t feu_arriere_position = 63;
-const uint8_t feu_arriere_stop = 255;
-const uint8_t feu_avant_croisement = 127;
-const uint8_t feu_avant_route = 255;
+#define frequence_clignotant 90
+#define feu_arriere_position 63
+#define feu_arriere_stop 255
+#define feu_avant_croisement 127
+#define feu_avant_route 255
 
 // Pour test
-const uint8_t diviseur_blanc = 20;
-const uint8_t diviseur_pause = 4;
-
-// Heure précédent clignotement
-uint32_t heure_clignotant;
+//#define diviseur_blanc(valeur_blanc) ((valeur_blanc) / 50)
+//#define diviseur_pause(valeur_pause) ((valeur_pause) / 4)
+// Mode normal
+#define diviseur_blanc(valeur_blanc) ((valeur_blanc))
+#define diviseur_pause(valeur_pause) ((valeur_pause))
 
 // Allume les feux avant et arrière de nuit
 void lumieres_nuit () {
-  analogWrite (feu_avant, feu_avant_croisement / diviseur_blanc);
+  analogWrite (feu_avant, diviseur_blanc (feu_avant_croisement));
   analogWrite (feu_arriere, feu_arriere_position);
 }
 
@@ -72,25 +81,9 @@ void lumieres_eteintes () {
   analogWrite (feu_arriere, 0);
 }
 
-// Calcule la pause en fonction du potentiomètre d'ajustement
-uint16_t calculer_pause (uint16_t duree_minimum) {
-  return map (analogRead (potentiometre_duree), 0, 1023, duree_minimum / diviseur_pause, duree_minimum * 2 / diviseur_pause);
-}
-
-// Pause d'une durée nominale, variable selon le potentiomètre
-void pause_variable (uint16_t duree_minimum) {
-  delay (calculer_pause (duree_minimum));
-}
-
-// Mémorise l'heure de début de clignotement
-void debut_clignotant () {
-  heure_clignotant = millis ();
-}
-
 // Pause entre deux clignotements
 void pause_clignotant () {
-  delay ((heure_clignotant + 60000 / frequence_clignotant / 2) - millis ());
-  debut_clignotant ();
+  delay (60000 / frequence_clignotant / 2);
 }
 
 // Actionne les feux de détresse allumés ou éteints
@@ -102,58 +95,58 @@ void actionne_feux_detresse (uint8_t etat) {
 
 // Répète un feu clignotant pendant une durée ajustable par potentiomètre
 void repeter_clignotant (uint8_t pin_clignotant, uint16_t duree_repeter) {
-  debut_clignotant ();
-  uint32_t fin_repeter = millis () + calculer_pause (duree_repeter);
-  do {
+  uint16_t nombre_boucle = duree_repeter / (60000 / frequence_clignotant);
+  uint16_t compteur_boucle;
+  for (compteur_boucle = 0 ; compteur_boucle < nombre_boucle ; ++compteur_boucle) {
     digitalWrite (pin_clignotant, HIGH);
     pause_clignotant ();
     digitalWrite (pin_clignotant, LOW);
     pause_clignotant ();
   }
-  while (millis () < fin_repeter);
-}
-
-// Répète les feux de détresse pendant une durée ajustable par potentiomètre
-void repeter_feux_detresse (uint16_t duree_repeter) {
-  debut_clignotant ();
-  uint32_t fin_repeter = millis () + calculer_pause (duree_repeter);
-  do {
-    actionne_feux_detresse (HIGH);
-    actionne_feux_detresse (LOW);
-  }
-  while (millis () < fin_repeter);
 }
 
 void setup() {
-  Serial.begin (115200);
-  Serial.println ("Scère d'éclairage d'une voiture, 8 configurations - 09/08/2024");
+  #ifdef avec_serial
+    Serial.begin (115200);
+    Serial.println ("Scéne d'éclairage d'une voiture, 8 configurations - 20/05/2024");
+  #endif
   pinMode (feu_avant, OUTPUT);
   pinMode (feu_arriere, OUTPUT);
   pinMode (clignotants_gauche, OUTPUT);
   pinMode (clignotants_droite, OUTPUT);
-  pinMode (configuration_1, INPUT_PULLUP);
-  pinMode (configuration_2, INPUT_PULLUP);
-  pinMode (configuration_3, INPUT_PULLUP);
-  pinMode (potentiometre_duree, INPUT);
+  #ifdef configuration_avec_potentiometre
+    pinMode (configuration_potentiometre_init, INPUT);
+  #else
+    pinMode (configuration_1, INPUT_PULLUP);
+    pinMode (configuration_2, INPUT_PULLUP);
+    pinMode (configuration_3, INPUT_PULLUP);
+  #endif
+  #ifdef avec_potentiometre_duree
+    pinMode (potentiometre_duree_init, INPUT);
+  #endif
   analogWrite (feu_avant, 0);
   analogWrite (feu_arriere, 0);
   digitalWrite (clignotants_gauche, LOW);
   digitalWrite (clignotants_droite, LOW);
-  debut_clignotant ();
 }
 
 void loop() {
   // Calcule la configuration choisie
-  uint8_t configuration_choisie = 0;
-  if (digitalRead (configuration_1) == HIGH) {
-    configuration_choisie |= 1;
-  }
-  if (digitalRead (configuration_2) == HIGH) {
-    configuration_choisie |= 2;
-  }
-  if (digitalRead (configuration_3) == HIGH) {
-    configuration_choisie |= 4;
-  }
+  uint8_t configuration_choisie;
+  #ifdef configuration_avec_potentiometre
+    configuration_choisie = analogRead (configuration_potentiometre_read) / 128;
+  #else
+    configuration_choisie = 0;
+    if (digitalRead (configuration_1) == HIGH) {
+      configuration_choisie |= 1;
+    }
+    if (digitalRead (configuration_2) == HIGH) {
+      configuration_choisie |= 2;
+    }
+    if (digitalRead (configuration_3) == HIGH) {
+      configuration_choisie |= 4;
+    }
+  #endif
 
   // Enchainement selon la configuration choisie
   // (on regroupe les configurations qui ont suffisement de points communs)
@@ -162,19 +155,19 @@ void loop() {
     case 5:
     // Lumières de nuit avec freinage
       lumieres_nuit ();
-      pause_variable (20000);
+      delay (diviseur_pause (20000));
       analogWrite (feu_arriere, feu_arriere_stop);
-      pause_variable (10000);
+      delay (diviseur_pause (10000));
       if (configuration_choisie == 5) {
     // Lumières de nuit avec freinage et appel de phares
         analogWrite (feu_arriere, feu_arriere_position);
-        pause_variable (20000);
-        analogWrite (feu_avant, feu_avant_route / diviseur_blanc);
-        delay (1000);
-        analogWrite (feu_avant, feu_avant_croisement / diviseur_blanc);
-        delay (1000);
-        analogWrite (feu_avant, feu_avant_route / diviseur_blanc);
-        delay (1000);
+        delay (diviseur_pause (20000));
+        analogWrite (feu_avant, diviseur_blanc (feu_avant_route));
+        delay (diviseur_pause (1000));
+        analogWrite (feu_avant, diviseur_blanc (feu_avant_croisement));
+        delay (diviseur_pause (1000));
+        analogWrite (feu_avant, diviseur_blanc (feu_avant_route));
+        delay (diviseur_pause (1000));
       }
       break;
     case 4:
@@ -189,24 +182,30 @@ void loop() {
       else {
         lumieres_nuit ();
       }
-      pause_variable (20000);
-      repeter_clignotant (clignotants_droite, 5000);
+      delay (diviseur_pause (20000));
+      repeter_clignotant (clignotants_droite, diviseur_pause (5000));
       analogWrite (feu_arriere, feu_arriere_stop);
-      repeter_clignotant (clignotants_droite, 10000);
+      repeter_clignotant (clignotants_droite, diviseur_pause (10000));
       analogWrite (feu_arriere, (configuration_choisie == 0) ? 0 : feu_arriere_position);
-      repeter_clignotant (clignotants_droite, 30000);
+      repeter_clignotant (clignotants_droite, diviseur_pause (30000));
       lumieres_eteintes ();
       if (configuration_choisie == 2) {
-        repeter_feux_detresse (60000);
+      // Répète les feux de détresse pendant une durée
+        uint16_t nombre_boucle = diviseur_pause (60000) / (60000 / frequence_clignotant);
+        uint16_t compteur_boucle;
+        for (compteur_boucle = 0 ; compteur_boucle < nombre_boucle ; ++compteur_boucle) {
+          actionne_feux_detresse (HIGH);
+          actionne_feux_detresse (LOW);
+        }
       }
       else {
-        pause_variable (60000);
+        delay (diviseur_pause (60000));
       }
       if (configuration_choisie != 0) {
         lumieres_nuit ();
       }
-      pause_variable (5000);
-      repeter_clignotant (clignotants_gauche, 20000);
+      delay (diviseur_pause (5000));
+      repeter_clignotant (clignotants_gauche, diviseur_pause (20000));
       break;
     case 3:
     // Lumières de nuit et feux de détresse permanents
@@ -217,18 +216,18 @@ void loop() {
     case 1:
     // Tourner à gauche la nuit (lumières de nuit, clignotant à gauche, freinage, toujours clignotant à gauche, lumières de nuit)
       lumieres_nuit ();
-      pause_variable (20000);
-      repeter_clignotant (clignotants_gauche, 5000);
+      delay (diviseur_pause (20000));
+      repeter_clignotant (clignotants_gauche, diviseur_pause (5000));
       analogWrite (feu_arriere, feu_arriere_stop);
-      repeter_clignotant (clignotants_gauche, 10000);
+      repeter_clignotant (clignotants_gauche, diviseur_pause (10000));
       analogWrite (feu_arriere, feu_arriere_position);
-      repeter_clignotant (clignotants_gauche, 30000);
-      pause_variable (40000);
+      repeter_clignotant (clignotants_gauche, diviseur_pause (30000));
+      delay (diviseur_pause (40000));
       break;
     default:
     // Lumières de nuit fixes
       lumieres_nuit ();
-      delay (10000);
+      delay (diviseur_pause (10000));
       break;
   }
 }
